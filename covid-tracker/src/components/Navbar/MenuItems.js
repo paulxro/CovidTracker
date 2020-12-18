0234d1704251fbba9048d5144d658030abe8c477
paulxro <paulandreialdea@yahoo.com>
2020-12-18 15:07:49 -0500

    Initial Commit

---
+++ covid-tracker/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Database',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Feedback',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'View Map',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
